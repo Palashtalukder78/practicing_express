ddb03d832f0ef1dc7031fd50ab3c3ac3d6f52310
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-14 11:27:09 +0600

    exploring express.static() for read data from root(public) folder

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+app.use(express.static(__dirname + '/public/'))
+
 app.get('/', (req, res) => {
     res.send('This is the Home page')
 })
 app.post('/', (req, res) => {
-    console.log(req.body.firstName)
     res.send('This is the Home page with post method')
 })
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("The running port is: 4000")
 })
