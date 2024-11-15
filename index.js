f869300242546529bef70c83aca68aee64f4bdb7
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-15 10:18:22 +0600

    exploring res.render with ejs

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const express = require('express')
 const app = express();
-const cookieParser = require('cookie-parser');
-const handler = require('./handler');
 
-const adminRoute = express.Router()
-app.use('/admin', adminRoute)
-app.use(express.json())
-app.use(cookieParser())
+app.set('view engine', 'ejs')
 
-adminRoute.get('/dashboard',(req,res)=>{
-    console.log(req.ip)
-    res.send('We are in admin dashboard')
-})
-
-
-
-app.get('/user/:id', handler)
-app.post('/user/', (req, res) => {
-    console.log(req.body)
-    res.send('This is the POST page')
+app.get('/about', (req, res) => {
+    res.render('pages/about')
 })
 
 app.listen(4000, () => {
