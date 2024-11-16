304e5c482df2217f61e8e626aa68ea6c5d36165e
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-16 09:38:56 +0600

    exploring middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express();
 
+const myMiddleware1 = (req,res,next)=>{
+    console.log('Middleware 1')
+    next()
+}
+const myMiddleware2 = (req,res,next)=>{
+    console.log('Middleware 2')
+    next()
+}
+
+app.use(myMiddleware1, myMiddleware2)
 
 app.get('/about', (req, res) => {
-    res.set('Platform', 'Fiverr and Upwork');
-    console.log(res.get('Platform'))
-    res.end()
+    res.send('About')
 })
 
 app.listen(4000, () => {
