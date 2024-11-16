c352db50eb37e113735005558188d0abc5dd933a
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-16 10:16:49 +0600

    Handle error-middleware

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 const app = express();
 
 const adminRouter = express.Router();
 
-
-
-
 const logger = (req, res, next) => {
     console.log(`${new Date(Date.now()).toLocaleString()} - ${req.method} - ${req.originalUrl} - ${req.protocol} - ${req.ip}`);
-    res.end()
+    throw new Error("This is an Error");
+    
 }
 adminRouter.use(logger)
-
 app.use('/admin', adminRouter)
-
 
 adminRouter.get('/dashboard', (req, res) => {
     res.send('DashBoard')
@@ -22,6 +20,14 @@ adminRouter.get('/dashboard', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('About')
 })
+
+
+const errorMiddleware = (err,req,res,next)=>{
+    console.log(err.message);
+    res.status(500).send('There was a server side error')
+}
+
+adminRouter.use(errorMiddleware);
 
 app.listen(4000, () => {
     console.log("The running port is: 4000")
