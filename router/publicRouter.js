7cf1f1544dd1e2e4987e785ba997ce9bf7c255f0
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-17 14:32:26 +0600

    requested url not-found error handleling

--- router/publicRouter.js
+++ router/publicRouter.js
@@ -2,8 +2,13 @@ const express = require('express');
 const adminRouter = require('./adminRouter');
 const publicRouter = express.Router();
 
-publicRouter.get('/', (req,res)=>{
+publicRouter.get('/', (req, res) => {
     res.send(a)
+})
+
+publicRouter.use((req,res,next)=>{
+    // res.status(404).send('Requested url is not found')
+    next('Requested url is not found')
 })
 
 //invisible default error handleing
@@ -12,9 +17,12 @@ publicRouter.get('/', (req,res)=>{
 }) */
 
 //overwrite
-publicRouter.use((err,req,res,next)=>{
-    console.log(err)
-    res.send("There was a server side error!")
+publicRouter.use((err, req, res, next) => {
+    if (err.message) {
+        res.status(500).send(err.message)
+    } else {
+        res.status(500).send("There was a server side error!")
+    }
 })
 
 module.exports = publicRouter;
