c96c00932a65069238ecc9800097288a6d45ea80
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-17 11:02:59 +0600

    Exploring router- router.route for routing short way

--- router/publicRouter.js
+++ router/publicRouter.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const publicRouter = express.Router();
 
-publicRouter.param((param, option)=> (req, res, next, val) => {
-    if(val === option){
+publicRouter
+    .route('/user')
+    .all((req,res,next)=>{
+        console.log('Middleware')
         next()
-    }else{
-        res.sendStatus(403)
-    }
-})
-
-publicRouter.param('user', '12')
-publicRouter.get('/:user', (req, res) => {
-    res.send(`Welcome Admin`)
-})
-publicRouter.get('/about', (req, res) => {
-    res.send('About')
-})
+    })
+    .get((req,res)=>{
+        res.send('Get About page')
+    })
+    .post((req,res)=>{
+        res.send('Post About page')
+    })
+    .put((req,res)=>{
+        res.send('Put About page')
+    })
+    .delete((req,res)=>{
+        res.send('Delete About page')
+    })
 
 module.exports = publicRouter;
