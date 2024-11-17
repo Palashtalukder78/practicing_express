7b5ee4a29d67d41c70f18bc432545f22a285eda3
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-17 15:07:07 +0600

    exploring error-handling -with custom asyncronous function

--- router/publicRouter.js
+++ router/publicRouter.js
@@ -5,13 +5,13 @@ const publicRouter = express.Router();
 
 
 publicRouter.get('/', (req, res, next) => {
-    fs.readFileSync("/file-does-not-exist", (err, data)=>{
-        if(err){
-            next(err)
-        }else{
-            res.send(data)
+    setTimeout(() => {
+        try {
+            console.log(a)
+        } catch (error) {
+            next(error)
         }
-    })
+    }, 100);
 });
 publicRouter.use((err, req,res, next)=>{
     if(res.headersSent){
