e1e2f7f0b86a7fec395bffef9837a2ebe638fb41
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-17 10:50:01 +0600

    exploring router object- router.param(more functionalities)

--- router/publicRouter.js
+++ router/publicRouter.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const publicRouter = express.Router();
 
-publicRouter.param('user', (req, res, next, id) => {
-    req.user = id === '1' ? "Admin" : 'Anonymous';
-    next();
+publicRouter.param((param, option)=> (req, res, next, val) => {
+    if(val === option){
+        next()
+    }else{
+        res.sendStatus(403)
+    }
 })
 
+publicRouter.param('user', '12')
 publicRouter.get('/:user', (req, res) => {
-    res.send(`Welcome ${req.user}`)
+    res.send(`Welcome Admin`)
 })
 publicRouter.get('/about', (req, res) => {
     res.send('About')
