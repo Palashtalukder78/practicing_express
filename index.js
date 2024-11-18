1bddbfc55a2280e3802df0aa7b1a97338de6a382
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-18 14:46:45 +0600

    exploring uploading file- single file

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
-const adminRouter = require('./router/adminRouter');
-const publicRouter = require('./router/publicRouter');
+const multer = require('multer')
+
+
+//File upload folder
+const UPLOAD_FOLDER = './uploads/'
+// prepare the final multer upload object
+const upload = multer({ dest: UPLOAD_FOLDER })
+
 
 const app = express();
 
-app.use('/', publicRouter);
-app.use('/admin', adminRouter)
-
+app.post("/", upload.single('avatar'), (req, res) => {
+    res.send('Hello word')
+})
 
 app.listen(4000, () => {
     console.log("The running port is: 4000")
