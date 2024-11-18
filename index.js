0dba654d0852642ea05bd6c32f345e799161cc68
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-18 18:34:13 +0600

    fully functional file/image upload functionalities

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const multer = require('multer')
+const path = require('path')
 
 
 //File upload folder
 const UPLOAD_FOLDER = './uploads/'
+
+// Multer diskStorage
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=> {
+        cb(null, UPLOAD_FOLDER);
+    },
+    filename: (req,file, cb)=>{
+        const fileExt = path.extname(file.originalname);
+        const fileName = file.originalname.replace(fileExt, '').toLocaleLowerCase().split(' ').join('-')+ '-' + Date.now();
+        cb(null, fileName + fileExt)
+    }
+})
 // prepare the final multer upload object
 const upload = multer({
-    dest: UPLOAD_FOLDER,
+    storage: storage,
     limits: {
         fileSize: 1000000,
     },
