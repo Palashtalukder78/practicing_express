76a9600c80289c58ef55d398363f034d778fb807
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-11-18 15:01:44 +0600

    with multiple field

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const upload = multer({ dest: UPLOAD_FOLDER })
 
 const app = express();
 
-app.post("/", upload.array('avatar', 3), (req, res) => {
+app.post("/", upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 5 }]), (req, res) => {
     res.send('Hello word')
 })
 
